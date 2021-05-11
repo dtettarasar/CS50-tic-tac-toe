9f905b09fe76906141d3eb0b616fc06f6064b09b
Dylan Tettarasar <dtettarasar@gmail.com>
2021-05-11 22:30:21 +0200

    get cases from HTML DOM

--- js/main.js
+++ js/main.js
@@ -5,18 +5,23 @@ function mainScript() {
     let popUp = document.querySelector("#pop-up");
     let onePGameBtn = document.querySelector("#play-1p");
     let twoPGameBtn = document.getElementById("play-2p");
+    let cases = Array.from(document.querySelectorAll("[id^=case]"));
+
+    console.log(cases);
 
     //constructor for the players
-    function Player(number, turn) {
+    function Player(number, turn, symbol) {
         this.number = number;
         this.turn = turn;
+        this.symbol = symbol;
+        this.choices = [];
     }
 
-    let playerOne = new Player(1, true);
-    let playerTwo = new Player(2, false);
+    let playerOne = new Player(1, true, "circle");
+    let playerTwo = new Player(2, false, "cross");
 
-    console.log(playerOne);
-    console.log(playerTwo);
+    console.log(playerOne.choices);
+    //console.log(playerTwo);
     
     function startGame(playerNum) {
         popUp.style.display = "none";
