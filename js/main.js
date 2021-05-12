f4345d71a4c916faca0414e93447a113919c07b0
Dylan Tettarasar <dtettarasar@gmail.com>
2021-05-12 16:30:55 +0200

    record all the cases that are already taken

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-console.log("hello world");
-
 function mainScript() {
 
     let popUp = document.querySelector("#pop-up");
     let onePGameBtn = document.querySelector("#play-1p");
     let twoPGameBtn = document.getElementById("play-2p");
     let cases = Array.from(document.querySelectorAll("[id^=case]"));
+    let chosenCases = [];
 
     const victories = [
         [0,1,2],
@@ -27,12 +26,15 @@ function mainScript() {
         this.addSymbol = function(caseId) {
 
             // make sure a player doesn't make a choice only once
-            if (!this.choices.includes(caseId)) {
+            if (!chosenCases.includes(caseId)) {
                 this.choices.push(caseId);
+                chosenCases.push(caseId);
             }
 
             console.log("player " + this.number + " choices:");
             console.log(this.choices);
+            console.log("chosen cases");
+            console.log(chosenCases);
         }
         this.updateTurn = function() {
             this.turn = !this.turn;
@@ -43,24 +45,32 @@ function mainScript() {
     let playerOne = new Player(1, true, "circle");
     let playerTwo = new Player(2, false, "cross");
 
+    function playersTurn() {
+        playerOne.updateTurn();
+        playerTwo.updateTurn();
+    }
+
     // handle a game
     function handleChoice(index) {
         // handle player's choice & turns
 
-        if (playerOne.turn && !playerTwo.choices.includes(index)) {
+        if (playerOne.turn && !chosenCases.includes(index)) {
             playerOne.addSymbol(index);
-        } else if (!playerOne.choices.includes(index)) {
+            playersTurn();
+
+        } else if (!chosenCases.includes(index)) {
             playerTwo.addSymbol(index);
+            playersTurn();
         }
 
-        playerOne.updateTurn();
-        playerTwo.updateTurn();
     }
 
     //check result after a players's choice
     function checkResult() {
         // check result, if a player won or if draw, each time after a player made a choice
-        console.log("todo");
+        if (chosenCases.length == 9) {
+            console.log("game over!");
+        }
     }
 
     function loadCaseEvnt() {
